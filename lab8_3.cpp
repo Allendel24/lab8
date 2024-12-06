5ca6967098995bb8153ebd30db826ce0eb55ceba
Film_ <pawich23013@gmail.com_>
2024-12-06 15:59:12 +0700

    lab8_3.cpp

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,13 +1,35 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string s;
+    int age;
+    float h;
+    long long int b;
+    cout << "Enter your age: ";
+    cin >> age;
+    if(age <= 25){
+        cout << "Enter your height: ";
+        cin >> h;
+        if(h < 100){s = "Chopper";}
+        else if(h < 180){s = "Usopp";}
+        else{
+            cout << "Enter your bounty: ";
+            cin >> b;
+            if(b > 1100000000){s = "Zoro";}
+            else{s = "Sanji";}
+        }
+    }
+    else if(age <= 60){
+        cout << "Enter your bounty: ";
+        cin >> b;
+        if(b > 500000000){s = "Jinbe";}
+        else{s = "Franky";}
+    }
+    else{s = "Brook";}
+    
+    cout << "Your character = " << s;
+    
+    return 0;
+}
